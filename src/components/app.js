0eec25ef8d991a094ce2ddffa9f07314a67efd69
Kenrick Guie <nyhunter77@yahoo.com>
2017-11-04 12:47:41 -0400

    initial - react webpack express less lazy-load

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import css from '../../public/style/style.less';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Homepage!</h1>
+      </div>
+    );
+  }
+}
