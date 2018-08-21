439fca013ded570a2554599c7ec6513ae093cd4f
Ken Guie <nyhunter77@yahoo.com>
2018-08-20 22:49:29 -0400

    tweaking my bio image and bio area

--- src/components/bio.js
+++ src/components/bio.js
@@ -4,12 +4,12 @@ class Bio extends Component {
 	render() {
 		return(
 			<div id="bio">
-				<h3>I love technology!</h3>
-				<p>I have a wealth of technological knowledge from my experience in the computer field as Tech Support, Sales Engineer, Ads / DFP Implementation Engineer!</p>
-				<p>The last 4 years as a Front End Engineer with knowledge in HTML / CSS / SASS / JavaScript / React JS / Node.js / Jquery / Bootstrap / Git / WordPress and MongoDB, I love to build beautiful things!</p>
-				<p>Recently, I've also attained skills in Apple's iOS and Swift along with the new ARKit.</p>
-				<p>I work in the world of Frontend the most and I love it. I would love to continue that in my career for years to come but I’d also like to try my hand at creating some enterprise Apple apps in the near future.</p>
-				<p>I want to keep working in the world of web and if anyone needs a website or modifications to your personal sites, send me an email and let’s see what we can do! As an FYI, this new home page is built in React JS and I’ll be seeing what I can add to it little by little to make it more intriguing and to show off some interesting things I can do</p>				   
+				<h1>I love technology!</h1>
+				<h3>I have a wealth of technological knowledge from my experience in the computer field as Tech Support, Sales Engineer, Ads / DFP Implementation Engineer!</h3>
+				<h3>The last 4 years as a Front End Engineer with knowledge in HTML / CSS / SASS / JavaScript / React JS / Node.js / Jquery / Bootstrap / Git / WordPress and MongoDB, I love to build beautiful things!</h3>
+				<h3>Recently, I've also attained skills in Apple's iOS and Swift along with the new ARKit.</h3>
+				<h3>I work in the world of Frontend the most and I love it. I would love to continue that in my career for years to come but I’d also like to try my hand at creating some enterprise Apple apps in the near future.</h3>
+				<h3>I want to keep working in the world of web and if anyone needs a website or modifications to your personal sites, send me an email and let’s see what we can do! As an FYI, this new home page is built in React JS and I’ll be seeing what I can add to it little by little to make it more intriguing and to show off some interesting things I can do</h3>				   
 			</div>
 		)
 	}
--- src/components/profileImage.js
+++ src/components/profileImage.js
@@ -5,9 +5,11 @@ class Image extends Component {
 	render() {
 		return(
 			<div id="profileImage">
-				<LazyLoad offsetVertical={500}>
-					<img src="//res.cloudinary.com/image77/image/fetch/dpr_auto,f_auto,q_auto,w_350/http://www.kenguie.com/images/newHomepage/newProfile.jpg" alt="It's me!"/>
-				</LazyLoad>
+				<div className='container'>
+					<LazyLoad offsetVertical={500}>
+						<img src="http://res.cloudinary.com/image77/image/fetch/dpr_auto,f_auto,q_auto,w_350/http://www.kenguie.com/images/newHomepage/newProfile.jpg" alt="It's me!"/>
+					</LazyLoad>
+				</div>
 			</div>
 		)
 	}
