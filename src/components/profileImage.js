2d42ade733e9393c1a8173a454078d7f9f1eb098
Ken Guie <nyhunter77@yahoo.com>
2018-08-25 10:54:22 -0400

    https secured

--- src/components/profileImage.js
+++ src/components/profileImage.js
@@ -7,7 +7,7 @@ class Image extends Component {
 			<div id="profileImage">
 				<div className='container'>
 					<LazyLoad offsetVertical={500}>
-						<img src="http://res.cloudinary.com/image77/image/fetch/dpr_auto,f_auto,q_auto,w_350/http://www.kenguie.com/images/newHomepage/newProfile.jpg" alt="It's me!"/>
+						<img src="https://res.cloudinary.com/image77/image/fetch/dpr_auto,f_auto,q_auto,w_350/https://www.kenguie.com/images/newHomepage/newProfile.jpg" alt="It's me!"/>
 					</LazyLoad>
 				</div>
 			</div>
