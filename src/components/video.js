5808339597ec7e84379c8df5911474aba0251de5
Ken Guie <nyhunter77@yahoo.com>
2018-08-29 22:42:07 -0400

    switch videos and remove download button

--- src/components/video.js
+++ src/components/video.js
@@ -5,8 +5,8 @@ class Video extends Component {
 		return(
 			<div id="video">
         <h1>Meet Ken Guie, hear about what he knows, what he wants to work on, and if he can help you!</h1>
-        <video id="content_video" className="video-js vjs-default-skin" controls preload="auto">
-          <source src="https://www.kenguie.com/bioVideo/BioVidForHomePage-web-8.19.18.mp4" type="video/mp4" />
+        <video id="content_video" className="video-js vjs-default-skin" controls preload="auto" controlsList="nodownload">
+          <source src="http://www.kenguie.com/bioVideo/BioVideo-web-8.19.18.mp4" type="video/mp4" />
         </video>
 			</div>
 		)
